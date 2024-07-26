3b65fd4e3d01b1ba2ee094ccc673ee9b8f0ca42a
rizzzzapr <150340138+rizzap1717@users.noreply.github.com>
2024-07-26 13:58:32 +0700

    new models

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\kategori;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class KategoriController extends Controller
