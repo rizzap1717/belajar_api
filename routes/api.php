3d7690422357edeeb8dafc01da0b71a3434a34c7
rizzzzapr <150340138+rizzap1717@users.noreply.github.com>
2024-07-26 16:10:51 +0700

    aktor

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::resource('kategori', KategoriController::class);
     Route::resource('genre', GenreController::class);
-    Route::resource('actor', AktorController::class);
+    Route::resource('aktor', AktorController::class);
     Route::resource('film', FilmController::class);
 });
 
